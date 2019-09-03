305e2898db3848eaf9c068a2d6a9334e8c586fb4
Yifeng Pan <pan.yifeng@yandex.com>
2019-09-03 12:28:59 -0600

    Update zbrka.cpp

--- kattis/todo/zbrka.cpp
+++ kattis/todo/zbrka.cpp
@@ -15,11 +15,64 @@ How many ways are there to
 	0 1 2 3 4 .. n
 */
 
-// Might be easier in Haskell.
+// not fast enough, need better algorithm
 
 #include <iostream>
+#include <cmath>
+
+long long int mod = pow(10, 9) + 7;
+
+long long int dp(int N, int C){
+	--N; // offset for indexing
+
+	long long int confusions[N + 1][C + 1]; // row is upto N, col is ways to sum confusion from the first N rows
+	// Both offset by one due to indexing
+
+	for(int c = 1; c <= C; ++c) // base case for n = 0
+		confusions[0][c] = 0;
+
+	for(int n = 0; n <= N; ++n) // base case for c = 0 and c = n = 0
+		confusions[n][0] = 1;
+
+	for(int n = 1; n <= N; ++n){
+		for(int c = 1; c <= C; ++c){
+
+			long long int sum = 0;
+
+			if(c <= n){
+				int i = 0;
+				while(c - i >= 0 && i <= n){
+					sum += confusions[n - 1][c - i];
+					sum %= mod;
+					++i;
+				}
+			}else{
+				sum = confusions[n][c - 1];
+				sum -= confusions[n - 1][c - n - 1];
+				sum += confusions[n - 1][c];
+				sum = (sum + mod) % mod; // cpp doesn't mod negatives properly
+			}
+
+			confusions[n][c] = sum;
+
+		}
+	}
+
+	/*
+	for(int n = 0; n <= N; ++n){
+		for(int c = 0; c <= C; ++c){
+			std::cout << confusions[n][c] << ' ';
+		}
+		std::cout << '\n';
+	}
+	*/
+
+	return confusions[N][C];
+}
 
 int main(){
 	int N, C;
+	std::cin >> N >> C;
+	std::cout << dp(N, C);
 	return 0;
 }
