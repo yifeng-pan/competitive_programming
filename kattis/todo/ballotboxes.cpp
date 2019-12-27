84a02fa4fc84f6bbd398defb5c1f52f29e2ddab2
Yifeng Pan <pan.yifeng@yandex.com>
2019-12-26 17:26:43 -0700

    Update ballotboxes.cpp

--- kattis/todo/ballotboxes.cpp
+++ kattis/todo/ballotboxes.cpp
@@ -6,50 +6,100 @@ https://open.kattis.com/problems/ballotboxes
 #include <vector>
 #include <cmath>
 #include <algorithm>
-using namespace std;
 
-struct City {
-	vector<int> box;
-	vector<double> ppb; // pop per box
+// Time limit exceeded
+// Use different sorting algor?
+
+struct City{
+	int pop;
+	int box = 1;
+	int ppb; // pop per box
 	City(int);
-	void sort_ppb();
+	void add(int); // Adds n boxes to the city
 };
 
-City::City(int N) {
-	int city_pop;
-	for (int i = 0; i < N; ++i) {
-		cin >> city_pop;
-		ppb.push_back(city_pop);
-		box.push_back(1);
+City::City(int n){
+	pop = n;
+	ppb = pop;
+	return;
+}
+
+void City::add(int n){
+	box += n;
+	ppb = ceil((double) pop / box);
+	return;
+}
+
+void swap(std::vector<City>& cities, int a, int b){
+	iter_swap(cities.begin() + a, cities.begin() + b);
+}
+
+int partition(std::vector<City>& cities, int low, int high){
+	int p = cities[high].ppb;
+	int i = low;
+	for(int j = low; j < high; ++j){
+		if(cities[j].ppb > p){
+			swap(cities, i, j);
+			++i;
+		}
 	}
-	sort(ppb.begin(), ppb.end());
+	swap(cities, i, high);
+	return i;
 }
 
-//Assuming only the last element is out of place
-void City::sort_ppb() {
-	//WIP
+// quick sort in reverse order
+void r_qsort(std::vector<City>& cities, int low, int high){
+	if(low < high){
+		int p = partition(cities, low, high);
+		r_qsort(cities, low, p - 1);
+		r_qsort(cities, p + 1, high);
+	}
+	return; 
 }
 
-void t_case(const int N, int B) {
-	City city = { N };
+// custom insertion sort in reverse order
+// Assumes only the first element is out of place
+void cr_isort(std::vector<City>& cities){
+	for(int i = 0; i < cities.size() - 1; ++i){
+		if(cities[i].ppb >= cities[i+1].ppb)
+			return;
+		else
+			swap(cities, i, i + 1);
+		
+	}
+}
+
+int t_case(const int N, int B) {
+	if(N == 1){
+		int p;
+		std::cin >> p;
+		return ceil((double) p / B);
+	}
+
+	std::vector<City> cities;
+	for(int i = 0; i < N; ++i){
+		int p;
+		std::cin >> p;
+		cities.push_back(City(p));
+	}
+	r_qsort(cities, 0, cities.size() - 1);
 	B -= N;
-	int end = city.ppb.size() - 1;
-	double& high_ppb = city.ppb[end];
-	int& high_box = city.box[end];
-	for (; B > 0; --B) {
-		high_ppb *= high_box;
-		++high_box;
-		high_ppb /= high_box;
-		city.sort_ppb();
+	while(B > 0){
+		int n = floor((double) cities[0].pop / cities[1].ppb) - cities[0].box;
+		if(n > B) n = B;
+		if(n < 1) n = 1;
+		cities[0].add(n);
+		B -= n;
+		cr_isort(cities);
 	}
-	cout << ceil(high_ppb) << "\n";
+	return cities[0].ppb;
 }
 
 int main()
 {
 	int N, B;
-	while (cin >> N >> B && N != -1 && B != -1) {
-		t_case(N, B);
+	while (std::cin >> N >> B && N != -1 && B != -1) {
+		std::cout << t_case(N, B) << '\n';
 	}
 	return 0;
 }
